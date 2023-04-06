989c94c3b117425c72e3303011bedfa6fe0163ed
g.valeev <g.valeev@xsolla.com>
2023-04-06 12:50:50 +0500

    Change func name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Argggh123/utils
+module github.com/Argggh123/utils/v2
 
 go 1.19
--- utils.go
+++ utils.go
@@ -1,6 +1,6 @@
 package utils
 
-func Contains(stringSlice []string, stringToFind string) bool {
+func InSlice(stringSlice []string, stringToFind string) bool {
 	for _, str := range stringSlice {
 		if str == stringToFind {
 			return true
